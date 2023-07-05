dbedc38c150cb0cecf5f2b3c9dad91dd178d4c54
Crowdbotics <engineering@crowdbotics.com>
2023-07-05 14:07:46 +0000

    Initial commit.

---
+++ backend/wandering_waterfall_97766/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for wandering_waterfall_97766 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wandering_waterfall_97766.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-waterfall-97766",
+  url: "https://wandering-waterfall-97766.botics.co",
+  api: "https://wandering-waterfall-97766.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
